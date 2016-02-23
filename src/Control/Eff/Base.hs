36b2a81f3d50c140e9869e1ec3d3d50035a18b59
Mitchell Rosen <mitchellwrosen@gmail.com>
2016-02-23 14:53:52 -0800

    Rename Lift to Base, add convenience funcs

---
+++ src/Control/Eff/Base.hs
@@ -0,0 +1,33 @@
+module Control.Eff.Base where
+
+import Control.Eff
+
+import Control.Monad
+import Control.Monad.ST
+
+
+data Base m x where
+  Lift :: m a -> Base m a
+
+
+lift :: (Base m :< eff) => m a -> Eff eff a
+lift m = inject (Lift m)
+
+-- | Unlike mtl 'lift' vs 'liftIO', this is actually just a synonym. There is
+-- only one "layer" to lift to - the rest of the effect.
+liftIO :: (Base IO :< eff) => IO a -> Eff eff a
+liftIO = lift
+
+liftST :: (Base (ST s) :< eff) => ST s a -> Eff eff a
+liftST = lift
+
+
+runBase :: Monad m => Eff (Base m :+ Void) a -> m a
+runBase (Pure a) = pure a
+runBase (Impure (InL (Lift m)) k) = m >>= runBase . k
+
+runIO :: Eff (Base IO :+ Void) a -> IO a
+runIO = runBase
+
+runST :: Eff (Base (ST s) :+ Void) a -> ST s a
+runST = runBase
